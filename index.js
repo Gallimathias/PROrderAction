eecd896a0d826046b6ad24493cff4930b06565be
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2020-01-27 17:07:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, info } from "@actions/core";
+import { GitHub } from "@actions/github";
 
-let token = core.getInput("repo-token", {required: true});
-let client = new github.GitHub(token);
+let token = getInput("repo-token", { required: true });
+let client = new GitHub(token);
+
+console.log("Hello World, this is a demo");
 
 client.pulls.list().then(request => {
-    request.data
-    .forEach(pr => {
-        core.info("PR " + pr.id + ": " + pr.state);
-    })
+  request.data.forEach(pr => {
+    try {
+      info("PR " + pr.id + ": " + pr.state);
+    } catch (error) {
+      console.log("An error occured: " + error);
+    }
+  });
 });
-
